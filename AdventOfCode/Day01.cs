6d640b6a6ad1611f3e037a159b4b4d4950f4db6e
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-01 10:17:16 +0100

    day 1 part 2

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -27,6 +27,10 @@ public class Day01 : BaseDay
             }
 
             position = position % 100;
+            if (position < 0)
+            {
+                position = 100 + position;
+            }
 
             if (position == 0)
             {
@@ -36,5 +40,40 @@ public class Day01 : BaseDay
         return new ValueTask<string>(result.ToString());
     }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        int position = 50;
+        int result = 0;
+
+        for (int i = 0; i < _input.Length; ++i)
+        {
+            int previousPosition = position;
+
+            int rotation = int.Parse(_input[i].Substring(1));
+            if (_input[i][0] == 'R')
+            {
+                position += rotation;
+            }
+            else
+            {
+                position -= rotation;
+            }
+
+            if (position > 0)
+            {
+                result += position / 100;
+            }
+            else
+            {
+                result += Math.Abs(position) / 100 + (previousPosition == 0 ? 0 : 1);
+            }
+
+            position = position % 100;
+            if (position < 0)
+            {
+                position = 100 + position;
+            }
+        }
+        return new ValueTask<string>(result.ToString());
+    }
 }
