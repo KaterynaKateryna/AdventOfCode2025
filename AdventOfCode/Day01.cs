e7cdb40328653f4569da38a53b852a2ef867ccd1
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-01 09:56:12 +0100

    day 1 part 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,39 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        int position = 50;
+        int result = 0;
+
+        for (int i = 0; i < _input.Length; ++i)
+        {
+            int rotation = int.Parse(_input[i].Substring(1));
+            if (_input[i][0] == 'R')
+            {
+                position += rotation;
+            }
+            else
+            { 
+                position -= rotation;
+            }
+
+            position = position % 100;
+
+            if (position == 0)
+            {
+                result++;
+            }
+        }
+        return new ValueTask<string>(result.ToString());
+    }
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
