c57f7b3487e0045285466f8cb3251f996afd574c
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-02 22:18:19 +0100

    day 2 part 2

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -25,7 +25,8 @@ public class Day02 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("");
+        long result = _input.Select(x => GetInvalidIdsSum2(x.start, x.end)).Sum();
+        return new(result.ToString());
     }
 
     private long GetInvalidIdsSum(string start, string end)
@@ -41,6 +42,45 @@ public class Day02 : BaseDay
             if (id.Length % 2 == 0 && id.Substring(0, id.Length / 2) == id.Substring(id.Length / 2))
             {
                 sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    private long GetInvalidIdsSum2(string start, string end)
+    {
+        long startId = long.Parse(start);
+        long endId = long.Parse(end);
+
+        long sum = 0;
+
+        for (long i = startId; i <= endId; ++i)
+        {
+            string id = i.ToString();
+            for (int j = 1; j <= id.Length / 2; ++j)
+            {
+                if (id.Length % j == 0)
+                { 
+                    int times = id.Length / j;
+                    string pattern = id.Substring(0, j);
+                    bool matches = true;
+                    for (int k = 1; k < times; k++)
+                    {
+                        string next = id.Substring(j * k, j);
+                        if (next != pattern)
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+
+                    if (matches)
+                    { 
+                        sum += i;
+                        break;
+                    }
+                }
             }
         }
 
