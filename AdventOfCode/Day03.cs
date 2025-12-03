e058575613d22af5ec75bc0204e3cb3dd84d3e05
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-03 20:30:38 +0100

    day 3 part 1

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode;
+
+public class Day03 : BaseDay
+{
+    private readonly int[][] _input;
+
+    public Day03()
+    {
+        string[] inputLines = File.ReadAllLines(InputFilePath);
+        _input = inputLines
+            .Select(l => l.Select(c => int.Parse(c.ToString())).ToArray())
+            .ToArray();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new (_input.Sum(GetMaxJoltage).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+
+    private int GetMaxJoltage(int[] bank)
+    {
+        int max = bank.Take(bank.Length - 1).Max();
+        int indexOfMax = bank.IndexOf(max);
+
+        int max2 = bank.Skip(indexOfMax + 1).Max();
+
+        return int.Parse(max.ToString() + max2.ToString());
+
+    }
+}
