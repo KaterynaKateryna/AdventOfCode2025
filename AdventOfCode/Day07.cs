af044f4b9f7dad95a9d2f2fb9ec15545c1f984e8
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-14 16:40:41 +0100

    day 7 part 2

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -1,5 +1,4 @@
-﻿
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public class Day07 : BaseDay
 {
@@ -12,44 +11,69 @@ public class Day07 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var start = new Position(0, _input[0].IndexOf('S'));
-        HashSet<Position> beams = new() { start };
-        int splits = 0;
-
-        for (int i = 1; i < _input.Length; ++i)
-        {
-            HashSet<Position> newBeams = new();
-            foreach (Position beam in beams)
-            { 
-                char nextChar = _input[i][beam.Y];
-                if (nextChar == '^')
-                { 
-                    // split
-                    splits++;
-
-                    if (beam.Y > 0)
-                    {
-                        newBeams.Add(new Position(i, beam.Y - 1));
-                    }
-                    if (beam.Y < _input[0].Length - 1)
-                    {
-                        newBeams.Add(new Position(i, beam.Y + 1));
-                    }
-                }
-                else
-                {
-                    newBeams.Add(new Position(i, beam.Y));
-                }
-            }
-            beams = newBeams;
-        }
+        (int splits, _) = Solve();
 
         return new(splits.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new("");
+        (_, long timelines) = Solve();
+
+        return new(timelines.ToString());
+    }
+
+    private (int splits, long timelines) Solve()
+    {
+        var start = new Position(0, _input[0].IndexOf('S'));
+        Dictionary<Position, long> beams = new() { { start, 1 } };
+        int splits = 0;
+
+        for (int i = 1; i < _input.Length; ++i)
+        {
+            Dictionary<Position, long> newBeams = new();
+            foreach (Position beam in beams.Keys)
+            {
+                char nextChar = _input[i][beam.Y];
+                if (nextChar == '^')
+                {
+                    // split
+                    splits++;
+
+                    if (beam.Y > 0)
+                    {
+                        var newPosition = new Position(i, beam.Y - 1);
+                        TryAdd(newBeams, newPosition, beams[beam]);
+                    }
+                    if (beam.Y < _input[0].Length - 1)
+                    {
+                        var newPosition = new Position(i, beam.Y + 1);
+                        TryAdd(newBeams, newPosition, beams[beam]);
+                    }
+                }
+                else
+                {
+                    var newPosition = new Position(i, beam.Y);
+                    TryAdd(newBeams, newPosition, beams[beam]);
+                }
+            }
+
+            beams = newBeams;
+        }
+
+        return (splits, beams.Values.Sum());
+    }
+
+    private void TryAdd(Dictionary<Position, long> dictionary, Position newPosition, long timelines)
+    {
+        if (dictionary.ContainsKey(newPosition))
+        {
+            dictionary[newPosition] += timelines;
+        }
+        else
+        {
+            dictionary[newPosition] = timelines;
+        }
     }
 
     private record Position(int X, int Y);
