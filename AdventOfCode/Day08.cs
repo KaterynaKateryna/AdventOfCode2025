41cf2cd50ad53828d88cbc48e3b877ac89b09c20
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-14 17:43:07 +0100

    day 8 part 2

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -1,5 +1,4 @@
-﻿
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public class Day08 : BaseDay
 {
@@ -11,30 +10,15 @@ public class Day08 : BaseDay
     {
         string[] lines = File.ReadAllLines(InputFilePath);
         _nodes = lines
-            .Select(l => new Node(int.Parse(l.Split(",")[0]), int.Parse(l.Split(",")[1]), int.Parse(l.Split(",")[2])))
+            .Select(l => new Node(long.Parse(l.Split(",")[0]), long.Parse(l.Split(",")[1]), long.Parse(l.Split(",")[2])))
             .ToArray();
     }
 
     public override ValueTask<string> Solve_1()
     {
-        Dictionary<double, (Node a, Node b)> distances = new();
-
-        for (int i = 0; i < _nodes.Length - 1; ++i)
-        {
-            for (int j = i + 1; j < _nodes.Length; ++j)
-            { 
-                var dist = CalculateDistance(_nodes[i], _nodes[j]);
-
-                if (distances.ContainsKey(dist))
-                { 
-                    throw new Exception("Duplicate distance found, not expected");
-                }
-                distances[dist] = (_nodes[i], _nodes[j]);
-            }
-        }
+        Dictionary<double, (Node a, Node b)> distances = CalculateDistances();
 
         var pairs = distances.OrderBy(kv => kv.Key).Take(_connections).Select(kv => kv.Value).ToList();
-
         var distinctNodes = pairs.SelectMany(p => new[] { p.a, p.b }).Distinct().ToList();
         List<HashSet<Node>> circuits = distinctNodes.Select(n => new HashSet<Node> { n }).ToList();
 
@@ -61,7 +45,31 @@ public class Day08 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("");
+        Dictionary<double, (Node a, Node b)> distances = CalculateDistances();
+
+        var pairs = distances.OrderBy(kv => kv.Key).Select(kv => kv.Value).ToList();
+        List<HashSet<Node>> circuits = _nodes.Select(n => new HashSet<Node> { n }).ToList();
+
+        long res = 0;
+        foreach (var pair in pairs)
+        {
+            var circuitA = circuits.FirstOrDefault(c => c.Contains(pair.a));
+            var circuitB = circuits.FirstOrDefault(c => c.Contains(pair.b));
+
+            if (circuitA != circuitB)
+            {
+                circuitA.UnionWith(circuitB);
+                circuits.Remove(circuitB);
+            }
+
+            if (circuits.Count == 1)
+            {
+                res = pair.a.X * pair.b.X;
+                break;
+            }
+        }
+
+        return new(res.ToString());
     }
 
     private double CalculateDistance(Node a, Node b)
@@ -69,5 +77,24 @@ public class Day08 : BaseDay
         return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2));
     }
 
-    private record Node(int X, int Y, int Z);
+    private Dictionary<double, (Node a, Node b)> CalculateDistances()
+    {
+        Dictionary<double, (Node a, Node b)> distances = new();
+        for (int i = 0; i < _nodes.Length - 1; ++i)
+        {
+            for (int j = i + 1; j < _nodes.Length; ++j)
+            {
+                var dist = CalculateDistance(_nodes[i], _nodes[j]);
+
+                if (distances.ContainsKey(dist))
+                {
+                    throw new Exception("Duplicate distance found, not expected");
+                }
+                distances[dist] = (_nodes[i], _nodes[j]);
+            }
+        }
+        return distances;
+    }
+
+    private record Node(long X, long Y, long Z);
 }
