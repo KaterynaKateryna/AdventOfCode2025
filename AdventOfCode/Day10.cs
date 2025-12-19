da57fe4267fbbf9c3df55dfc245c222c11aac010
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-12-19 16:54:45 +0100

    add day 10 boilerplate; add ignore for rider

---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode;
+
+public class Day10 : BaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        return new("one");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("two");
+    }
+}
